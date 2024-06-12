afc9540401a81ad51fb1f90ac5108e5f8b047ac6
kundanponugumati <ponugumatikundansai@gmail.com>
2024-06-12 11:06:39 +0300

    Initial commit

---
+++ src/main/java/com/telusko/JobSeekerApplicationLobApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekerApplicationLobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekerApplicationLobApplication.class, args);
+	}
+
+}
